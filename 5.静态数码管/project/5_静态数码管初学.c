ea7a70bf24d3e993cf561eb3c451bf4f7db9d88a
BugCreator_YJY <omg58462@outlook.com>
2026-03-16 14:38:54 +0800

    Add files via upload

---
+++ 5.静态数码管/project/5_静态数码管初学.c
@@ -0,0 +1,32 @@
+
+#include <REGX52.H>
+
+unsigned int nixietable[] = 
+{0x3f,0x06,0x5b,0x4f,
+ 0x66,0x6d,0x7d,0x07,
+ 0x7f,0x6f,0x77,0x7c,
+ 0x39,0x5e,0x79,0x71};
+
+void nixie(int Location,int Number)
+{
+		switch(Location)
+		{
+			case 1:P2_4 = 1;P2_3 = 1;P2_2 = 1;break;
+			case 2:P2_4 = 1;P2_3 = 1;P2_2 = 0;break;
+			case 3:P2_4 = 1;P2_3 = 0;P2_2 = 1;break;
+			case 4:P2_4 = 1;P2_3 = 0;P2_2 = 0;break;
+			case 5:P2_4 = 0;P2_3 = 1;P2_2 = 1;break;
+			case 6:P2_4 = 0;P2_3 = 1;P2_2 = 0;break;
+			case 7:P2_4 = 0;P2_3 = 0;P2_2 = 1;break;
+			case 8:P2_4 = 0;P2_3 = 0;P2_2 = 0;break;
+		}
+		P0 = nixietable[Number];
+	
+}
+void main()
+{
+	
+		nixie(7,4);
+	while(1);
+	  
+}
