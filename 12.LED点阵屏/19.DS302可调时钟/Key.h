eb0ac215f845fbf3a95b381c3a7d2f689a73734d
BugCreator_YJY <omg58462@outlook.com>
2026-03-26 20:41:47 +0800

    Delete 12.LED点阵屏/19.DS302可调时钟 directory

--- 12.LED点阵屏/19.DS302可调时钟/Key.h
+++
@@ -1,9 +0,0 @@
-#ifndef __KEY_H__
-#define __KEY_H__
-
-
-
-//声明返回值类型为unsigned char 的函数Key
-unsigned char Key();
-
-#endif
--- 12.LED点阵屏/19.DS302可调时钟/Timer0.h
+++
@@ -1,7 +0,0 @@
-#ifndef __TIMER0_H__
-#define __TIMER0_H__
-
-void Timer0_Init(void);//@1ms
-
-
-#endif
